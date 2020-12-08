d1c59a99669680e1a02bde4992e50b5b47d4dfa4
Mohammad Sahil <sahilmejakhas@gmail.com>
2020-12-08 23:35:14 +0530

    landing page layout complete

---
+++ src/script.js
@@ -0,0 +1,97 @@
+// navbar 
+function openFunction(){
+    document.getElementById("menu").style.transform="translateX(0)";
+}
+function closeFunction(){
+    document.getElementById("menu").style.transform="translateX(-100%)";
+}
+
+
+//swiper carousel
+// Params
+
+var sliderSelector = '.swiper-container',
+    options = {
+      init: false,
+      loop: true,
+      speed:800,
+      slidesPerView: 1, // or 'auto'
+      //spaceBetween: -10,
+      centeredSlides : true,
+      effect: 'coverflow', // 'cube', 'fade', 'coverflow',
+      coverflowEffect: {
+        rotate: 0, // Slide rotate in degrees
+        stretch: 0, // Stretch space between slides (in px)
+        depth: 0, // Depth offset in px (slides translate in Z axis)
+        modifier: 0, // Effect multipler
+        slideShadows : true, // Enables slides shadows
+        //autoplay: 5000,
+        speed: 800,
+        spaceBetween: 0,
+        autoplayDesableOnInteraction: true
+      },
+      grabCursor: true,
+      parallax: true,
+      autoplay: 500,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        1023: {
+          slidesPerView: 1,
+          spaceBetween: 0
+        }
+      },
+      // Events
+      on: {
+        imagesReady: function(){
+          this.el.classList.remove('loading');
+        }
+      }
+    };
+
+    
+
+
+    var mySwiper = new Swiper ('.swiper-container', 
+	{
+    speed:1000,
+		direction: 'horizontal',
+		zoom: true,
+		keyboard: 
+		{
+			enabled: true,
+			onlyInViewport: false,
+		},
+		mousewheel: 
+		{
+			invert: true,
+		},
+    autoplay: 
+    {
+      delay: 2000,
+    },
+    loop: true,
+	});
+
+
+
+
+
+
+
+
+
+var mySwiper = new Swiper(sliderSelector, options);
+
+// Initialize slider
+mySwiper.init();
+
+
+
+//swiper carousel ends
