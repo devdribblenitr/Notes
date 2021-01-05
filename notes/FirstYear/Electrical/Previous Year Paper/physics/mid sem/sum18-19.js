124c47b35d09496516850d88ef435b69c64eddb2
Padmaja Bhol <brpadmaja224@gmail.com>
2021-01-06 01:26:08 +0530

    summer updated

--- notes/FirstYear/Electrical/Previous Year Paper/physics/mid sem/sum18-19.js
+++ notes/FirstYear/Electrical/Previous Year Paper/physics/mid sem/sum18-19.js
@@ -13,7 +13,7 @@ function closeFunction(){
 
 //scripts that fetch pdf from dropbox through adobe sdk api
 
-const dropboxLink = "https://www.dropbox.com/s/g6410ebw9dfojg5/math%20note%20up-to%20LMVT.pdf?dl=0";
+const dropboxLink = "https://www.dropbox.com/s/kbnwarw7l2wn1vh/PH1001%20mid%2019.pdf?dl=0";
 const clientId = "9df0d2ddd369458eaf116fcddc4bf6a8";
 const viewerOptions = {
     embedMode: "FULL_WINDOW",
