d1e06645859253db7c5021444ab95c5008d5013e
Mohammad Sahil <sahilmejakhas@gmail.com>
2021-02-20 18:32:47 +0530

    clubs and societies added

---
+++ clubs/src/script.js
@@ -0,0 +1,84 @@
+// Counter Section
+function inVisible(element) {
+    var WindowTop = $(window).scrollTop();
+    var WindowBottom = WindowTop + $(window).height();
+    var ElementTop = element.offset().top;
+    var ElementBottom = ElementTop + element.height();
+    if ((ElementBottom <= WindowBottom) && ElementTop >= WindowTop)
+      animate(element);
+   }
+   
+   function animate(element) {
+    if (!element.hasClass('ms-animated')) {
+      var maxval = element.data('max');
+      var html = element.html();
+      element.addClass("ms-animated");
+      $({
+        countNum: element.html()
+      }).animate({
+        countNum: maxval
+      }, {
+        duration: 2000,
+        easing: 'linear',
+        step: function() {
+          element.html(Math.floor(this.countNum) + html);
+        },
+        complete: function() {
+          element.html(this.countNum + html);
+        }
+      });
+    }
+   
+   }
+   
+   $(function() {
+    $(window).scroll(function() {
+      $("h2[data-max]").each(function() {
+        inVisible($(this));
+      });
+    })
+   });
+
+
+
+
+
+
+
+
+
+
+
+
+var counted = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (counted == 0 && $(window).scrollTop() > oTop) {
+    $('.count').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    counted = 1;
+  }
+
+});
