aaeac4adb0a62e7b61cf34913455c103e785d665
Mohammad Sahil <sahilmejakhas@gmail.com>
2021-05-29 10:01:57 +0530

    added all the notes and books

--- notes/src/script.js
+++ notes/src/script.js
@@ -42,3 +42,16 @@ var mySwiper = new Swiper ('.swiper-separeator2', {
 
 
 /*cards swiper js for trade and oters card ends here*/
+//search box
+$('.search-toggle').addClass('closed');
+
+$('.search-toggle .search-icon').click(function(e) {
+  if ($('.search-toggle').hasClass('closed')) {
+    $('.search-toggle').removeClass('closed').addClass('opened');
+    $('.search-toggle, .search-container').addClass('opened');
+    $('#search-terms').focus();
+  } else {
+    $('.search-toggle').removeClass('opened').addClass('closed');
+    $('.search-toggle, .search-container').removeClass('opened');
+  }
+});
